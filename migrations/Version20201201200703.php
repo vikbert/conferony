decfbfe05705877a949c6dfabc37da161f1e59c8
Xun Zhou <segentor@gmail.com>
2020-12-01 21:25:05 +0100

    migrations

--- migrations/Version20201201200703.php
+++ migrations/Version20201201200703.php
@@ -1,32 +1,22 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20201201200703 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function up(Schema $schema): void
     {
-        return '';
-    }
-
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE conference_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE conference (id INT NOT NULL, city VARCHAR(255) NOT NULL, year VARCHAR(4) NOT NULL, is_international BOOLEAN NOT NULL, PRIMARY KEY(id))');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE conference_id_seq CASCADE');
         $this->addSql('DROP TABLE conference');
