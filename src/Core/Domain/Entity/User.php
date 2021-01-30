66b149381750042f3b27dcc9475a07f0b39b3178
Xun Zhou <segentor@gmail.com>
2021-01-30 22:52:09 +0100

    updates

---
+++ src/Core/Domain/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core\Domain\Entity;
+
+use App\Infrastructure\Doctrine\Repository\UserRepositoryInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ src/Infrastructure/Doctrine/Repository/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Infrastructure\Doctrine\Repository;
+
+use App\Core\Domain\Entity\User;
+use App\Core\Domain\Repository\UserRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[] findAll()
+ * @method User[] findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function findOneByUsername(string $username): User
+    {
+        return $this->findOneBy(['username' => $username]);
+    }
+}
